e3734b95bf4e5a55b131f8bc956092fbce038b70
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2025-05-06 11:53:36 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -27,8 +27,8 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
-        // 仅选择黑木耳源和豆瓣资源
-        selectedAPIs = ["heimuer", "dbzy"];
+        // 仅选择电影天堂源和豆瓣资源
+        selectedAPIs = ["dytt", "dbzy"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
         
         // 默认选中过滤开关
